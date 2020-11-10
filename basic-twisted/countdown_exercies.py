21a83f4d840f5a332259eb3a214a109e034d314f
Nenu1985 <nenuzhny85@gmail.com>
2020-11-10 14:32:32 +0300

    excersice

--- basic-twisted/countdown_exercies.py
+++ basic-twisted/countdown_exercies.py
@@ -1,28 +1,34 @@
 
 from twisted.internet import reactor
-from twisted.internet.task import LoopingCall
+
 class Countdown:
-    def __init__(self, n):
+    def __init__(self, n: int, counters_state: list, current_counter_idx: int):
 
         self.counter = n
+        self.counters_state = counters_state
+        self.current_counter_idx = current_counter_idx
 
     def count(self):
+        if self.counter == 0:
+            # reactor.stop()
+            self.counters_state[self.current_counter_idx] = True
+            self.counter_finished()
+        else:
+            print(self.counter, '...')
+            self.counter -= 1
+            reactor.callLater(1, self.count)
 
-        print(self.counter, '...')
-        self.counter -= 1
-        return self.counter
+    def counter_finished(self):
+        if len(list(filter(None, self.counters_state))) == len(self.counters_state):
+            print('That\'s all')
+            reactor.stop()
 
 
 from twisted.internet import reactor
-c1 = Countdown(5)
-c1_loop = LoopingCall(c1.count)
-
-c1_loop.start(1.0)
-
-# while True:
-#     print(c1.counter)
-
-
+num_of_counters = 3
+conters_state = [False] * num_of_counters
+for i in range(num_of_counters):
+    reactor.callWhenRunning(Countdown(5, conters_state, i).count)
 # reactor.callWhenRunning(Countdown(4).count)
 print('Start!')
 reactor.run()
