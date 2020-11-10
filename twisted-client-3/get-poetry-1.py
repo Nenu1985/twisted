6d562498f4f62dd4911c800fd895464461f8b341
Nenu1985 <nenuzhny85@gmail.com>
2020-11-10 12:51:14 +0300

    part 6 to v3

--- twisted-client-3/get-poetry-1.py
+++ twisted-client-3/get-poetry-1.py
@@ -31,7 +31,7 @@ for that to work.
     _, addresses = parser.parse_args()
 
     if not addresses:
-        print parser.format_help()
+        print(parser.format_help())
         parser.exit()
 
     def parse_address(addr):
@@ -46,15 +46,15 @@ for that to work.
 
         return host, int(port)
 
-    return map(parse_address, addresses)
+    return list(map(parse_address, addresses))
 
 
 class PoetryProtocol(Protocol):
 
     poem = ''
 
-    def dataReceived(self, data):
-        self.poem += data
+    def dataReceived(self, data: bytes):
+        self.poem += data.decode()
 
     def connectionLost(self, reason):
         self.poemReceived(self.poem)
@@ -108,7 +108,7 @@ def poetry_main():
         poem_done()
 
     def poem_failed(err):
-        print >>sys.stderr, 'Poem failed:', err
+        print('Poem failed:', err, file=sys.stderr)
         errors.append(err)
         poem_done()
 
@@ -123,7 +123,7 @@ def poetry_main():
     reactor.run()
 
     for poem in poems:
-        print poem
+        print(poem)
 
 
 if __name__ == '__main__':
